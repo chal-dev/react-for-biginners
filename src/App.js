ce3bcdad78a34b846900a55a810cdb3b25d93485
mjjeong <mjjeong@sgrsoft.com>
2022-01-27 17:41:22 +0900

    5강

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import Button from "./Button"
+// import Button from "./Button"
 import styles from "./App.module.css";
-
+import {useState, useEffect} from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("")
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run all the time");
+  const iRunOnlyOnce = () =>{
+    console.log("i run only one time")
+  };
+  useEffect(()=>{
+    console.log('one')
+  },[]);
+  useEffect(()=>{
+    console.log('search for ', keyword)
+  },[keyword]);
+  console.log("search fo r",keyword);
   return (
     <div>
-      <h1 className={styles.title}>환영합니다!</h1>
-      <Button text={"continue"}/>
+      <input type="text" onChange={onChange} value={keyword} />
+      <h1 className={styles.title}>{counter}!</h1>
+      <button onClick={onClick} text={"continue"}>click me</button>
     </div>
   );
 }
