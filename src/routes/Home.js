9f43adc118aaf11d2b658b6ea20064326ef5f843
mjjeong <mjjeong@sgrsoft.com>
2022-02-15 14:54:27 +0900

    #7

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,5 @@
 import {useEffect, useState } from "react";
-import Movie from "./components/Movie";
+import Movie from "../components/Movie";
 
 
 function Home(){
@@ -25,6 +25,7 @@ function Home(){
           {movies.map((movie) => ( 
             <Movie 
               key ={movie.id}
+              id={movie.id}
               coverImg={movie.medium_cover_image} 
               title={movie.title} 
               summary={movie.summary}
@@ -34,3 +35,5 @@ function Home(){
         }
     </div>;
 }
+
+export default Home;
